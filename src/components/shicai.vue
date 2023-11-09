c9b6805da33518aec75fb9e1b9059cf1b52b34fa
wubing <wubing@subconit.com>
2023-11-09 23:30:43 +0800

    feat:修改宽高

--- src/components/shicai.vue
+++ src/components/shicai.vue
@@ -7,38 +7,40 @@
         class="top-image"
         @click="handleEdit"
       />
-      <div class="el-icon-edit-box" @click="handleEdit">
-        <i class="el-icon-edit"></i>
-      </div>
-      <div style="padding: 14px" v-show="edit">
+      <div class="edit-box" v-show="edit">
+        <div class="row-item">
+          <span>名称</span>
+          <el-input
+            size="mini"
+            type="age"
+            v-model="form.currentName"
+            autocomplete="off"
+          ></el-input>
+        </div>
+        <div class="row-item">
+          <span>单位</span>
+          <el-input
+            type="age"
+            size="mini"
+            v-model="form.unit"
+            autocomplete="off"
+          ></el-input>
+        </div>
+        <div class="row-item">
+          <span>类型</span>
+          <div class="tag-box">
+            <el-tag size="mini" v-for="tag in selectOptions" :key="tag.value">{{
+              tag.value
+            }}</el-tag>
+          </div>
+        </div>
         <div class="bottom clearfix">
-          <div>
-            <span>名称</span>
-            <el-input
-              type="age"
-              v-model="form.currentName"
-              autocomplete="off"
-            ></el-input>
-          </div>
-          <div>
-            <span>单位</span>
-            <el-input
-              type="age"
-              v-model="form.unit"
-              autocomplete="off"
-            ></el-input>
-          </div>
-          <div>
-            <span>类型</span>
-            <div class="tag-box">
-              <el-tag
-                size="mini"
-                v-for="tag in selectOptions"
-                :key="tag.value"
-                >{{ tag.value }}</el-tag
-              >
-            </div>
-          </div>
+          <el-button type="text" class="button" @click="saveCard"
+            >保存</el-button
+          >
+          <el-button type="text" class="button" @click="deleteCard"
+            >删除</el-button
+          >
         </div>
       </div>
       <div v-show="!edit">
@@ -83,6 +85,12 @@ export default {
     };
   },
   methods: {
+    saveCard() {
+      console.log("保存");
+    },
+    deleteCard() {
+      console.log("删除");
+    },
     handleEdit() {
       this.edit = !this.edit;
     },
@@ -98,10 +106,18 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
 .food-card {
-  font: 14px;
+  width: 268px;
+  height: 280px;
+  font-size: 14px;
   position: relative;
+  .edit-box {
+    position: relative;
+    padding: 30px 14px 14px 14px;
+  }
   .el-card {
     font-size: 14px;
+    width: 100%;
+    height: 100%;
   }
   .el-form-item__label {
     font-size: 12px;
@@ -113,7 +129,8 @@ export default {
     cursor: pointer;
     position: absolute;
     right: 12px;
-    bottom: 32px;
+    top: 0;
+    font-size: 28px;
   }
   .bottom-title {
     font-size: 16px;
@@ -122,6 +139,12 @@ export default {
     display: flex;
     justify-content: space-between;
     padding: 5px 15px;
+  }
+  .row-item {
+    display: flex;
+    span {
+      font-size: 12px;
+    }
   }
 }
 .text {
